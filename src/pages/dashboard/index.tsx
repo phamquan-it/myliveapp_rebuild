db2f1cc0d55164f6913031c074ceb3557090c483
Pham Quan <c2202lm.pmquan@aptech.vn>
2024-06-17 00:01:58 +0700

    add chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,7 +6,7 @@ const Line = dynamic(() => import("react-chartjs-2").then((mod) => mod.Line), {
 });
 import DashBoardLayout from "@/components/admin/DashBoardLayout";
 import _ from "lodash-es";
-const data:any = {
+const data: any = {
   labels: ["January", "February", "March", "April", "May"],
   datasets: [
     {
@@ -15,31 +15,140 @@ const data:any = {
       fill: false,
       borderColor: "rgb(75, 192, 192)",
       tension: 0.1,
-      
+
     },
   ],
 };
 import { GetStaticPropsContext } from "next";
 import { Pie } from "react-chartjs-2";
 import FastInfo from "@/components/admin/FastInfo";
-import { YoutubeFilled } from "@ant-design/icons";
+import { FilterFilled, YoutubeFilled } from "@ant-design/icons";
 import FastInfoList from "@/components/admin/FastInfoList";
+import { Input, Switch, Table } from "antd";
+import { useTranslations } from "use-intl";
+import PlatformSelectForFilter from "@/components/admin/PlatformSelectForFilter";
+import Title from "antd/es/typography/Title";
+import CategorySelect from "@/components/admin/CategorySelect";
 
 const Page = () => {
+  const t = useTranslations("MyLanguage")
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: t('entryno'),
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: t("account"),
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: t("service"),
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: t('amount'),
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: t('status'),
+      dataIndex: 'key',
+      key: 'key',
+      render: (text: any, record: any) => <Switch
+        defaultChecked={text == 1}
+      />
+    },
+    {
+      title: t('createAt'),
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
   return (
     <>
       <div
-      
+
       >
-        <FastInfoList/>
-       <div className="flex">
-       
-        <div className="w-2/3"><Line data={data} /></div>
-        <div className="w-80">
-        <Pie data={dataPie} />
+        <FastInfoList />
+        <div className="flex">
+
+          <div className="w-2/3">
+          <Line data={data} options={{
+            plugins:{
+          
+              legend:{
+                display:false
+              }
+            }
+          }}/></div>
+          <div className="w-80">
+            <Pie data={dataPie} options={{
+              plugins: {
+                legend: {
+                  display: true,
+                  position: 'right',
+                },
+              },
+            }} />
+          </div>
         </div>
-       </div>
       </div>
+      <div className="mt-4">
+        <Title level={3} className="text-center">Orders</Title>
+
+      </div>
+      <div className="border shadow-sm rounded-md overflow-hidden my-2 hidden">
+        <div className="bg-gray-100  px-2 py-1 pt-2 ">
+          <Title level={5} className="!font-semibold">
+            <FilterFilled /> Filter
+          </Title>
+        </div>
+        <div className="p-3  flex gap-2">
+          <Input placeholder={t('searchplh')} style={{
+            width: 200
+          }} />
+
+          <PlatformSelectForFilter />
+          <PlatformSelectForFilter />
+        </div>
+      </div>
+      <Table className="border rounded-md" pagination={false} scroll={{ y: 150 }} dataSource={dataSource} columns={columns} />
     </>
   );
 };
@@ -54,9 +163,11 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 }
 
 const dataPie = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+
+  labels: ["Youtube", "Facebook", "Slack", "Green", "Purple", "Orange"],
   datasets: [
     {
+
       label: "# of Votes",
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
